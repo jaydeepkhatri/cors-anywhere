67167ab362273d880e51d20a4ec9002ed0d328f0
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2023-03-04 15:33:07 +0530

    feat: Initial files

---
+++ index.js
@@ -0,0 +1,10 @@
+const cors_proxy = require('cors-anywhere');
+
+// Port
+const port = process.env.PORT || 3000;
+
+cors_proxy.createServer({
+  originWhitelist: [], // Allowing all origins
+}).listen(port, () => {
+  console.log(`CORS Anywhere server running on ${port}`);
+});
